b93badbe5b524ef1a6a6594d02550a0a00a9d8e3
quis121 <qaisafana0@gmail.com>
2025-07-22 23:26:27 +0300

    Add files via upload

---
+++ mobile_shop_project_organized/LoginResponse.java
@@ -0,0 +1,48 @@
+package com.mobileshop;
+
+public class LoginResponse {
+    private boolean success;
+    private String message;
+    private User user;
+    private String token;
+    
+    public LoginResponse(boolean success, String message, User user, String token) {
+        this.success = success;
+        this.message = message;
+        this.user = user;
+        this.token = token;
+    }
+    
+    public boolean isSuccess() {
+        return success;
+    }
+    
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    public String getToken() {
+        return token;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
